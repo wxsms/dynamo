4a27c02c8203ba5df8035b008ab4ab8c82c81218
Anant Sharma <anants@nvidia.com>
2025-09-09 13:22:16 -0400

    test: remove nighlty marker in kvbm tests (#2958)
    Signed-off-by: Anant Sharma <anants@nvidia.com>

--- tests/kvbm/test_determinism.py
+++ tests/kvbm/test_determinism.py
@@ -35,9 +35,7 @@ pytestmark = [
     pytest.mark.kvbm,
     pytest.mark.e2e,
     pytest.mark.slow,
-    pytest.mark.nightly,
     pytest.mark.gpu_1,
-    pytest.mark.skip,  # TODO failing for me so turning off for now
 ]
 
 
