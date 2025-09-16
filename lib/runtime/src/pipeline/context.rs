02a22cbcf35697347b182b634767ced6aeb0cc10
Graham King <grahamk@nvidia.com>
2025-09-16 13:13:11 -0400

    chore(runtime): Shorten the license header (#3059)
    Signed-off-by: Graham King <grahamk@nvidia.com>

--- lib/runtime/src/pipeline/context.rs
+++ lib/runtime/src/pipeline/context.rs
@@ -1,25 +1,5 @@
 // SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//! Context Module
-//!
-//! There are two context object defined in this module:
-//!
-//! - [`Context`] is an input context which is propagated through the processing pipeline,
-//!   up to the point where the input is pass to an [`crate::engine::AsyncEngine`] for processing.
-//! - [`StreamContext`] is the input context transformed into to a type erased context that maintains the inputs
-//!   registry and visitors. `StreamAdaptors` will amend themselves to the [`StreamContext`] to allow for the
 
 use std::ops::{Deref, DerefMut};
 use std::sync::{Arc, Mutex};
