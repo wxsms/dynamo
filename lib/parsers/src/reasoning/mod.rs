0e63cd9c3766375a0f219826e07a18bc88c0152c
nachiketb-nvidia <nachiketb@nvidia.com>
2025-09-19 14:17:11 -0700

    feat: add granite class of reasoning parser (#2936)
    Signed-off-by: nachiketb <nachiketb@nvidia.com>

--- lib/parsers/src/reasoning/mod.rs
+++ lib/parsers/src/reasoning/mod.rs
@@ -5,10 +5,12 @@ use std::sync::OnceLock;
 
 mod base_parser;
 mod gpt_oss_parser;
+mod granite_parser;
 
 // Re-export main types and functions for convenience
 pub use base_parser::BasicReasoningParser;
 pub use gpt_oss_parser::GptOssReasoningParser;
+pub use granite_parser::GraniteReasoningParser;
 
 static REASONING_PARSER_MAP: OnceLock<HashMap<&'static str, ReasoningParserType>> = OnceLock::new();
 
@@ -24,6 +26,7 @@ fn get_reasoning_parser_map() -> &'static HashMap<&'static str, ReasoningParserT
         map.insert("kimi", ReasoningParserType::Kimi);
         map.insert("step3", ReasoningParserType::Step3);
         map.insert("mistral", ReasoningParserType::Mistral);
+        map.insert("granite", ReasoningParserType::Granite);
         map
     })
 }
@@ -87,6 +90,7 @@ pub enum ReasoningParserType {
     NemotronDeci,
     Kimi,
     Mistral,
+    Granite,
 }
 
 #[derive(std::fmt::Debug)]
@@ -165,6 +169,9 @@ impl ReasoningParserType {
                     }
                 }
             },
+            ReasoningParserType::Granite => ReasoningParserWrapper {
+                parser: Box::new(GraniteReasoningParser::new()),
+            },
         }
     }
 
@@ -205,6 +212,7 @@ mod tests {
             "kimi",
             "step3",
             "mistral",
+            "granite",
         ];
         for parser in available_parsers {
             assert!(parsers.contains(&parser));
