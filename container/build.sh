bba41321cf7cafa751e5680b0862507ece5383e6
Keiven C <213854356+keivenchang@users.noreply.github.com>
2025-11-10 20:11:59 -0800

    fix: use latest tag across all branches for version detection (#4229)
    Signed-off-by: Keiven Chang <keivenchang@users.noreply.github.com>
    Co-authored-by: Keiven Chang <keivenchang@users.noreply.github.com>

--- container/build.sh
+++ container/build.sh
@@ -32,7 +32,7 @@ commit_id=$(git rev-parse --short HEAD)
 current_tag=$(git describe --tags --exact-match 2>/dev/null | sed 's/^v//') || true
 
 # Get latest TAG and add COMMIT_ID for dev
-latest_tag=$(git describe --tags --abbrev=0 "$(git rev-list --tags --max-count=1 main)" | sed 's/^v//') || true
+latest_tag=$(git describe --tags --abbrev=0 "$(git rev-list --tags --max-count=1)" | sed 's/^v//') || true
 if [[ -z ${latest_tag} ]]; then
     latest_tag="0.0.1"
     echo "No git release tag found, setting to unknown version: ${latest_tag}"
