09bb1c68cc38036ad3b9f9507d57884e5e64ce05
Ryan Olson <ryanolson@users.noreply.github.com>
2025-11-17 16:01:11 -0700

    feat: kvbm kernels (#4356)
    Signed-off-by: Ryan Olson <rolson@nvidia.com>

--- lib/bindings/kvbm/python/kvbm/__init__.py
+++ lib/bindings/kvbm/python/kvbm/__init__.py
@@ -6,3 +6,4 @@
 from kvbm._core import BlockManager as BlockManager
 from kvbm._core import KvbmLeader as KvbmLeader
 from kvbm._core import KvbmWorker as KvbmWorker
+from kvbm._core import kernels as kernels
