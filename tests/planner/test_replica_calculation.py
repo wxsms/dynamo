2fa3627aa61c22f0bf1d4fc933a4107286be88da
Hongkuan Zhou <tedzhouhk@gmail.com>
2025-12-30 16:32:03 -0800

    test: fix missing args in replica calculation test (#5113)
    Signed-off-by: hongkuanz <hongkuanz@nvidia.com>

--- tests/planner/test_replica_calculation.py
+++ tests/planner/test_replica_calculation.py
@@ -40,6 +40,8 @@ from dynamo.planner.utils.planner_core import Metrics, Planner  # noqa: E402
 
 
 @pytest.fixture
+@pytest.mark.pre_merge
+@pytest.mark.gpu_0
 def planner():
     """Set up test environment with mocked dependencies."""
     # Create mock arguments
@@ -54,7 +56,8 @@ def planner():
     args.backend = "vllm"
     args.no_operation = True  # Don't actually scale
     args.no_correction = False  # Allow correction factors
-    args.prometheus_port = 0  # 0 means disabled
+    args.metric_pulling_prometheus_endpoint = "http://localhost:9090"  # dummy endpoint
+    args.metric_reporting_prometheus_port = 0  # 0 means disabled
     args.load_predictor = "constant"
     args.load_prediction_window_size = 10
     args.profile_results_dir = os.path.join(
