e6ad5bc5a3d950f97115d2af9d1c213ec8aa99a9
milesial <milesial@users.noreply.github.com>
2026-01-23 18:07:34 -0800

    fix: incremental build for dynamo-llm (#5601)
    Signed-off-by: Alexandre Milesi <milesial@users.noreply.github.com>

--- lib/llm/build.rs
+++ lib/llm/build.rs
@@ -50,7 +50,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn build_protos() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .type_attribute(".", "#[derive(serde::Serialize,serde::Deserialize)]")
-        .compile_protos(&["kserve.proto"], &["src/grpc/protos"])?;
+        .compile_protos(&["src/grpc/protos/kserve.proto"], &["src/grpc/protos"])?;
     Ok(())
 }
 
