6cb76b967a58cd53c2250bf46a22f59c533ecec3
Qi Wang <qiwa@nvidia.com>
2026-02-04 12:43:21 -0800

    feat: introduce cuda_ipc for TRT-LLM PrefillHandler (#5773)

--- components/src/dynamo/trtllm/multimodal/__init__.py
+++ components/src/dynamo/trtllm/multimodal/__init__.py
@@ -1,6 +1,10 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+from .cuda_ipc import extract_embeddings_from_handles
 from .hasher import MultimodalHasher
 
-__all__ = ["MultimodalHasher"]
+__all__ = [
+    "MultimodalHasher",
+    "extract_embeddings_from_handles",
+]
