2ac22df738d6566d732c15eb34a2ef668d73be21
Graham King <grahamk@nvidia.com>
2026-04-15 14:20:06 -0400

    chore: Remove unused vLLM multimodal chat processor (#8229)
    Signed-off-by: Graham King <grahamk@nvidia.com>

--- components/src/dynamo/vllm/multimodal_utils/__init__.py
+++ components/src/dynamo/vllm/multimodal_utils/__init__.py
@@ -4,11 +4,6 @@
 from dynamo.common.multimodal.http_client import get_http_client
 from dynamo.common.multimodal.image_loader import ImageLoader
 from dynamo.vllm.multimodal_utils.chat_message_utils import extract_user_text
-from dynamo.vllm.multimodal_utils.chat_processor import (
-    ChatProcessor,
-    CompletionsProcessor,
-    ProcessMixIn,
-)
 from dynamo.vllm.multimodal_utils.encode_utils import (
     encode_image_embeddings,
     get_embedding_hash,
@@ -30,9 +25,6 @@ from dynamo.vllm.multimodal_utils.protocol import (
 )
 
 __all__ = [
-    "ChatProcessor",
-    "CompletionsProcessor",
-    "ProcessMixIn",
     "encode_image_embeddings",
     "extract_user_text",
     "get_encoder_components",
